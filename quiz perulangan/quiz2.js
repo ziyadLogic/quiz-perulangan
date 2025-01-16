e0f98330f158dab5cb2219f6157f21d73a4350ee
ziyadLogic <mardanispd9@gmail.com>
2025-01-16 11:16:17 +0700

    Add files via upload

---
+++ quiz perulangan/quiz2.js
@@ -0,0 +1,5 @@
+let perkalian = 5;
+
+for(let i = 0; i <=10; i++) {
+    console.log( perkalian * i);
+}
---
+++ quiz perulangan/quiz3.js
@@ -0,0 +1,6 @@
+let celsius = 1;
+let farenheit = (celsius * 9/5) + 32;
+
+console.log(farenheit);
+
+//pak, ini gak pake perulangan soalnya tinggal masukin rumus farenheit hehe... -fadil
